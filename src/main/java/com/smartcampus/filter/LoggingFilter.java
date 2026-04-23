2d71e1aabd424abe1d957f776fd87e21978d4368
ViuslaS16 <www.vss99@gmail.com>
2026-04-23 09:41:42 +0530

    Add LoggingFilter to trace HTTP requests and responses globally

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,31 @@
+package com.smartcampus.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger logger = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+        logger.info("REQUEST: " + method + " " + uri);
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+        int status = responseContext.getStatus();
+        logger.info("RESPONSE: " + method + " " + uri + " -> " + status);
+    }
+}
